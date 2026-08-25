475f7a0818caf952346b07ee2852eda00c1fa2f2
Ayush Kapoor <71757760+mrlempticles@users.noreply.github.com>
2026-08-25 23:51:12 +0530

    Time: 0 ms (100%), Space: 20.7 MB (54.24%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+
+            // Farthest position we can reach
+            farthest = max(farthest, i + nums[i]);
+
+            // We have reached the end of our current jump
+            if (i == currentEnd) {
+
+                jumps++;
+
+                // Start a new jump
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+};
