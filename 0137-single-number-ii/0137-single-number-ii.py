e413c73af3b729f41104593e70d005f5df69cad9
Ayush Kapoor <71757760+mrlempticles@users.noreply.github.com>
2026-09-01 23:45:14 +0530

    Time: 35 ms (18.19%), Space: 13.3 MB (88.93%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+
+        for i in range(32):
+            count = 0
+
+            for num in nums:
+                if (num >> i) & 1:
+                    count += 1
+
+            if count % 3:
+                result |= (1 << i)
+
+        # Handle negative numbers
+        if result >= 2**31:
+            result -= 2**32
+
+        return result
