3fb1c1cbb73786a5a9fa76b689eb589770d1f0e8
Ayush Kapoor <71757760+mrlempticles@users.noreply.github.com>
2026-09-03 23:40:42 +0530

    Time: 0 ms (100%), Space: 12.6 MB (24.76%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,16 @@
+
+class Solution(object):
+    def subsets(self, nums):
+        result = [[]]
+
+        for num in nums:
+            new_subsets = []
+
+            for subset in result:
+                new_subsets.append(subset + [num])
+
+            result += new_subsets
+
+        return result
+
+        
